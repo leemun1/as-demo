338855289a8db6f4add33546f63f87f4f8572d61
Mike Lee <mikemunkyu.lee@gmail.com>
2020-03-07 20:47:40 -0500

    Initial commit - try calling functions defined in the ASM module

---
+++ demo.js
@@ -0,0 +1,6 @@
+// ./src/index.ts
+const asm = require('./index');
+
+console.log('Running Assembly Script Demo: ')
+console.log('add', asm.add(300, 2));
+console.log('subtract', asm.subtract(300, 2));
